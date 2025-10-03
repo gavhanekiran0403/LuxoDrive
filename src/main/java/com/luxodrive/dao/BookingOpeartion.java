0f37321808f81d3a0dc01094e0824a90ed2fec75
Kiran Gavhane <gavhanekiran0403@gmail.com>
2025-10-03 23:11:37 +0530

    getting  BookingOpeartion

--- src/main/java/com/luxodrive/dao/BookingOpeartion.java
+++ src/main/java/com/luxodrive/dao/BookingOpeartion.java
@@ -3,12 +3,15 @@ package com.luxodrive.dao;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.List;
 
 import com.luxodrive.jdbc.connection.DBConnection;
+import com.luxodrive.model.AdminNotification;
 import com.luxodrive.model.Booking;
 import com.luxodrive.model.Car;
+import com.luxodrive.model.Driver;
 import com.luxodrive.model.User;
 
 public class BookingOpeartion {
@@ -29,6 +32,9 @@ public class BookingOpeartion {
 				int carId = rs.getInt("car_id");
 				Car car = CarOperation.getCarById(carId);
 				
+				int driverId = rs.getInt("driver_id");
+				Driver driver = DriverOperation.getDriverById(driverId);
+				
 				Booking booking = new Booking(
 						rs.getInt("booking_id"), 
 						rs.getString("pickup_location"), 
@@ -38,9 +44,11 @@ public class BookingOpeartion {
 						rs.getString("return_date"), 
 						rs.getString("driving_option"), 
 						rs.getDouble("total_amount"), 
-						rs.getString("status"), 
+						rs.getString("status"),
+						rs.getTimestamp("created_at"),
 						user, 
-						car
+						car,
+						driver
 						);
 				bookings.add(booking);
 			}
@@ -52,11 +60,11 @@ public class BookingOpeartion {
 	
 	public static int addBooking(Booking booking) {
 		
-		int row = 0;
+		int generatedId = 0;
 		try(Connection conn = DBConnection.getConnection()){
 			String query = "INSERT INTO bookings(pickup_location, drop_location, pickup_date, pickup_Time, return_date, "
-					+ "driving_option, total_amount, status, user_id, car_id) values(?,?,?,?,?,?,?,?,?)";
-			PreparedStatement psmt = conn.prepareStatement(query);
+					+ "driving_option, total_amount, status, user_id, car_id) values(?,?,?,?,?,?,?,?,?,?)";
+			PreparedStatement psmt = conn.prepareStatement(query, Statement.RETURN_GENERATED_KEYS);
 			psmt.setString(1, booking.getPickupLocation());
 			psmt.setString(2, booking.getDropLocation());
 			psmt.setString(3, booking.getPickupDate());
@@ -68,12 +76,20 @@ public class BookingOpeartion {
 			psmt.setInt(9, booking.getUser().getUserId());
 			psmt.setInt(10, booking.getCar().getCarId());
 			
-			row = psmt.executeUpdate();
+			int rows = psmt.executeUpdate();
+
+	        if (rows > 0) {
+	            try (ResultSet rs = psmt.getGeneratedKeys()) {
+	                if (rs.next()) {
+	                    generatedId = rs.getInt(1);
+	                }
+	            }
+	        }
 			
 		}catch(Exception e) {
 			e.printStackTrace();
 		}
-		return row;
+		return generatedId;
 	}
 	
 	public static int updateBooking(Booking booking) {
@@ -107,18 +123,47 @@ public class BookingOpeartion {
 		Booking booking = null;
 		
 		try(Connection conn = DBConnection.getConnection()){
-			String query = "SELECT * FROM bookings WHERE booking_id = ?";
+			String query = "SELECT b.*, u.*, c.* FROM bookings b "
+					+ "JOIN users u ON b.user_id = u.user_id "
+					+ "JOIN cars c ON b.car_id = c.car_id WHERE booking_id = ?";
 			PreparedStatement psmt = conn.prepareStatement(query);
 			psmt.setInt(1, bookingId);
 			ResultSet rs =  psmt.executeQuery();
 			while(rs.next()) {
 				
-				int userId = rs.getInt("user_id");
-				User user = UserOperation.getUserById(userId);
-						
-				int carId = rs.getInt("car_id");
-				Car car = CarOperation.getCarById(carId);
+				User user = new User();
+				user.setUserId(rs.getInt("user_id"));
+	            user.setFullName(rs.getString("full_Name"));
+	            user.setPhoneNo(rs.getString("phone_no"));
+	            user.setEmail(rs.getString("email"));
+	            user.setPassword(rs.getString("password"));
+                user.setConfirmPassword(rs.getString("confirmpassword"));
+                user.setRole(rs.getString("role"));
+                user.setStatus(rs.getString("status"));
+                
+                List<byte[]> images = new ArrayList<byte[]>();
+
+				images.add(rs.getBytes("image1"));
+				images.add(rs.getBytes("image2"));
+				images.add(rs.getBytes("image3"));
+				images.add(rs.getBytes("image4"));
 				
+	            Car car = new Car();
+	            car.setCarId(rs.getInt("car_id"));
+	            car.setModelName(rs.getString("model_name"));
+	            car.setCarNo(rs.getString("car_no"));
+	            car.setFuelType(rs.getString("fuel_type"));
+	            car.setSeats(rs.getInt("seats"));
+	            car.setRent(rs.getDouble("rent"));
+	            car.setCarYearModel(rs.getInt("car_year_model"));
+	            car.setInsuranceNo(rs.getString("insurance_no"));
+	            car.setFeatures(rs.getString("features"));
+	            car.setStatus(rs.getString("status"));
+	            car.setImages(images);
+	            
+	            int driverId = rs.getInt("driver_id");
+				Driver driver = DriverOperation.getDriverById(driverId);
+	            			
 				booking = new Booking(
 						rs.getInt("booking_id"), 
 						rs.getString("pickup_location"), 
@@ -128,9 +173,11 @@ public class BookingOpeartion {
 						rs.getString("return_date"), 
 						rs.getString("driving_option"), 
 						rs.getDouble("total_amount"), 
-						rs.getString("status"), 
+						rs.getString("status"),
+						rs.getTimestamp("created_at"),
 						user, 
-						car
+						car,
+						driver
 						);
 				
 			}
@@ -151,5 +198,143 @@ public class BookingOpeartion {
 			e.printStackTrace();
 		}
 		return row;
+	}
+	
+	public static boolean updateStatus(int bookingId, String status) {
+	    String sql = "UPDATE bookings SET status=? WHERE booking_id=?";
+	    try (Connection con = DBConnection.getConnection();
+	         PreparedStatement psmt = con.prepareStatement(sql)) {
+	    	psmt.setString(1, status);
+	    	psmt.setInt(2, bookingId);
+	    	int updated = psmt.executeUpdate();
+	        return updated > 0;
+	    } catch (Exception e) {
+	        e.printStackTrace();
+	        return false;
+	    }
+	}
+	
+	public static List<AdminNotification> getUnreadNotifications() {
+	    List<AdminNotification> notifications = new ArrayList<>();
+	    String sql = "SELECT * FROM adminnotifications WHERE is_read = FALSE ORDER BY created_at DESC";
+
+	    try (Connection conn = DBConnection.getConnection();
+	         PreparedStatement psmt = conn.prepareStatement(sql)) {
+
+	        ResultSet rs = psmt.executeQuery();
+	        while (rs.next()) {
+	        	
+	        	int userId = rs.getInt("user_id");
+	        	User user = UserOperation.getUserById(userId);
+                
+                int bookingId = rs.getInt("booking_id");
+                Booking booking = BookingOpeartion.getBookingById(bookingId);
+                
+                AdminNotification notification = new AdminNotification();
+                notification.setNotificationId(rs.getInt("notification_id"));
+                notification.setMessage(rs.getString("message"));
+                notification.setCreatedAt(rs.getTimestamp("created_at"));
+                notification.setIsRead((rs.getString("is_read")));
+                notification.setUser(user);
+                notification.setBooking(booking);
+
+
+	            notifications.add(notification);
+	        }
+
+	    } catch (Exception e) {
+	        e.printStackTrace();
+	    }
+
+	    return notifications;
+	}
+	
+	public static void markNotificationAsRead(int bookingId) {
+	    try(Connection conn = DBConnection.getConnection()) {
+	        String sql = "UPDATE adminnotifications SET is_read = TRUE WHERE booking_id = ?";
+	        PreparedStatement ps = conn.prepareStatement(sql);
+	        ps.setInt(1, bookingId);
+	        ps.executeUpdate();
+	    } catch(Exception e) { e.printStackTrace(); }
+	}
+
+	public static Booking getLatestBookingForUser(String userId) {
+        Booking booking = null;
+
+        String sql = "SELECT * FROM bookings WHERE user_id = ? ORDER BY created_at DESC LIMIT 1";
+
+        try (Connection conn = DBConnection.getConnection();
+             PreparedStatement psmt = conn.prepareStatement(sql)) {
+
+        	psmt.setString(1, userId);
+        	ResultSet rs = psmt.executeQuery();
+            while(rs.next()) {
+	        	User user = new User();
+				user.setUserId(rs.getInt("user_id"));
+	            user.setFullName(rs.getString("full_Name"));
+	            user.setPhoneNo(rs.getString("phone_no"));
+	            user.setEmail(rs.getString("email"));
+	            user.setPassword(rs.getString("password"));
+                user.setConfirmPassword(rs.getString("confirmpassword"));
+                user.setRole(rs.getString("role"));
+                user.setStatus(rs.getString("status"));
+                
+                List<byte[]> images = new ArrayList<byte[]>();
+
+				images.add(rs.getBytes("image1"));
+				images.add(rs.getBytes("image2"));
+				images.add(rs.getBytes("image3"));
+				images.add(rs.getBytes("image4"));
+				
+	            Car car = new Car();
+	            car.setCarId(rs.getInt("car_id"));
+	            car.setModelName(rs.getString("model_name"));
+	            car.setCarNo(rs.getString("car_no"));
+	            car.setFuelType(rs.getString("fuel_type"));
+	            car.setSeats(rs.getInt("seats"));
+	            car.setRent(rs.getDouble("rent"));
+	            car.setCarYearModel(rs.getInt("car_year_model"));
+	            car.setInsuranceNo(rs.getString("insurance_no"));
+	            car.setFeatures(rs.getString("features"));
+	            car.setStatus(rs.getString("status"));
+	            car.setImages(images);
+	            
+	            int driverId = rs.getInt("driver_id");
+				Driver driver = DriverOperation.getDriverById(driverId);
+				
+				booking = new Booking(
+						rs.getInt("booking_id"), 
+						rs.getString("pickup_location"), 
+						rs.getString("drop_location"), 
+						rs.getString("pickup_date"), 
+						rs.getString("pickup_Time"), 
+						rs.getString("return_date"), 
+						rs.getString("driving_option"), 
+						rs.getDouble("total_amount"), 
+						rs.getString("status"),
+						rs.getTimestamp("created_at"),
+						user, 
+						car,
+						driver
+						);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return booking;
+    }
+	
+	public static boolean addDriverToBooking(int bookingId, int driver) {
+	    String sql = "UPDATE bookings SET driver_id=? WHERE booking_id=?";
+	    try (Connection con = DBConnection.getConnection();
+	         PreparedStatement psmt = con.prepareStatement(sql)) {
+	    	psmt.setInt(1, driver);
+	    	psmt.setInt(2, bookingId);
+	        return psmt.executeUpdate() > 0;
+	    } catch (Exception e) {
+	        e.printStackTrace();
+	        return false;
+	    }
 	}
 }
